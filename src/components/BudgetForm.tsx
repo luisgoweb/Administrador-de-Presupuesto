40beb8b7e3c7a1eb6f74a60ec62beeea07fe90c6
luisgoweb <luismga2012@gmail.com>
2025-07-08 13:15:52 -0400

    BudgetForm.tsx: Se valida formulario, y se crea función para enviar los datos

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,24 @@
-
+import { useState } from "react"
 
 const BudgetForm = () => {
+    const [budget, setBudget] = useState(0)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValidForm = () => {
+        return budget > 0
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        console.log('enviando presupuesto de:', budget)
+    }
+
   return (
-    <form className="space-y-3">
+    <form className="space-y-3" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-3">
             <label htmlFor="budget" className="text-2xl text-blue-600 text-center font-bold">Define tu presupuesto</label>
             <input 
@@ -10,13 +26,16 @@ const BudgetForm = () => {
             type="number"
             placeholder="Ej. 300 o 500"
             className="border border-slate-300 p-2 rounded-lg w-full"
+            value={budget}
+            onChange={handleChange}
             />
         </div>
 
         <input 
         type="submit"
         value="Guardar Presupuesto"
-        className="bg-blue-600 hover:bg-blue-900 cursor-pointer p-2 text-white uppercase rounded-lg w-full"
+        className="bg-blue-600 hover:bg-blue-900 cursor-pointer p-2 text-white uppercase rounded-lg w-full disabled:opacity-10"
+        disabled={!isValidForm()}
         />
     </form>
   )
