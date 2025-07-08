025550477b7847aaaf1854be570bd537f113e79c
luisgoweb <luismga2012@gmail.com>
2025-07-08 13:32:09 -0400

    Se crea carpeta context con el archivo BudgetContext.tsx con la configuracion del context y provider

---
+++ src/context/Budget-Context.tsx
@@ -0,0 +1,29 @@
+import { createContext, useReducer, type Dispatch, type ReactNode } from "react";
+import { BudgetReducer, initialState, type BudgetActions, type BudgetState } from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+    state: BudgetState
+    dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+
+export const BudgetProvider = ({children}: BudgetProviderProps) => {
+    const[state, dispatch] = useReducer(BudgetReducer, initialState)
+
+    return (
+        <BudgetContext.Provider
+        value={{
+            state,
+            dispatch
+        }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    )
+}
