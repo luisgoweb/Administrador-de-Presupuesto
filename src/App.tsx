4eb26783bb3a5a3ea2e560751cd1fd4db852dcd5
luisgoweb <luismga2012@gmail.com>
2025-07-08 11:57:37 -0400

    Inicio del proyecto con vite, react, typescript y tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+  return (
+    <>
+      <header className="bg-blue-600 p-3 max-h-72">
+          <h1 className="text-2xl text-center font-bold text-white">Planificador de Gastos</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
