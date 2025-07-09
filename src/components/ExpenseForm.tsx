3f1de695e4838df055037207ea8fb05ab4ac3a30
luisgoweb <luismga2012@gmail.com>
2025-07-08 21:52:16 -0400

    Se crea componente ExpenseForm.tsx para agregar los gastos

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,11 @@
+
+
+const ExpenseForm = () => {
+  return (
+    <form>
+        <legend className="text-2xl text-blue-600 text-center">Formulario</legend>
+    </form>
+  )
+}
+
+export default ExpenseForm
