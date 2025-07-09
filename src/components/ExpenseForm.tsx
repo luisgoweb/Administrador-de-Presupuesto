2c123cb6aebac16ae68a408904cfecb35624ed13
luisgoweb <luismga2012@gmail.com>
2025-07-09 10:03:25 -0400

    Crea carpeta data con el archivo categories.ts

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,65 @@
+import { categories } from "../data/categories"
 
 
 const ExpenseForm = () => {
   return (
-    <form>
-        <legend className="text-2xl text-blue-600 text-center">Formulario</legend>
+    <form className="space-y-3">
+        <legend className="text-2xl uppercase font-bold text-blue-600 text-center border-b-4 border-blue-600">Nuevo Gasto</legend>
+
+        <div className="grid grid-cols-1 gap-2 mt-5">
+            <label htmlFor="expenseName">Nombre del Gasto:</label>
+            <input 
+            id="expenseName"
+            className="w-full border border-slate-300 p-2 rounded-lg"
+            type="text"
+            placeholder="Escribe el nombre del gasto"
+            name="expenseName"
+            />
+        </div>
+
+        <div className="grid grid-cols-1 gap-2 mt-5">
+            <label htmlFor="amount">Cantidad:</label>
+            <input 
+            id="amount"
+            className="w-full border border-slate-300 p-2 rounded-lg"
+            type="number"
+            placeholder="Ej. 300 o 500"
+            name="amount"
+            />
+        </div>
+
+        <div className="grid grid-cols-1 gap-2 mt-5">
+            <label htmlFor="category">Categoría:</label>
+            <select 
+            id="category"
+            name="category"
+            className="w-full border border-slate-300 p-2 rounded-lg"
+            > 
+                <option value="">-- Seleccione --</option>
+                {categories.map(category => (
+                    <option key={category.id} value={category.id}>
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1 gap-2 mt-5">
+            <label htmlFor="date">Fecha:</label>
+            <input 
+            id="date"
+            className="w-full border border-slate-300 p-2 rounded-lg"
+            type="date"
+            name="date"
+            />
+        </div>
+
+        <input 
+        type="submit"
+        value="Registrar Gasto"
+        className="bg-blue-600 hover:bg-blue-800 text-white cursor-pointer uppercase font-bold rounded-lg w-full p-2"
+        />
+
     </form>
   )
 }
