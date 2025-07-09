31665323383510f1a12b74aa621e68e3b0fa346f
luisgoweb <luismga2012@gmail.com>
2025-07-08 21:19:56 -0400

    BudgetTracker.tsx: Se inserta imagen de grafico de gasotos y boton para el reset app

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -2,8 +2,16 @@
 
 const BudgetTracker = () => {
   return (
-    <div>
-        <h2>Tu presupuesto</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex justify-center ">
+            <img src="/grafico.jpg" alt="Grafico de Gastos" />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-8">
+            <button type="button" className="bg-pink-600 hover:bg-pink-900 text-white p-2 uppercase cursor-pointer w-full rounded-lg">
+                Reset App
+            </button>
+        </div>
+        
     </div>
   )
 }
